8e86a98edf2ff33a76bc46c49f7ebc366d776828
Annafuran <33253216+Annafuran@users.noreply.github.com>
2017-11-15 17:11:45 +0100

    Add files via upload
    Gamla filer som innehåller gammal kod :)))

--- src/index.js
+++ src/index.js
@@ -1,8 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './app';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App/>, document.body ); // getElementById('root')
-registerServiceWorker();
+
+
+var http = require('http'), fs = require('fs');
+
+
+fs.readFile('./index.html', function (err, html) {
+    if (err) {
+        throw err; 
+    }  
+	
+    http.createServer(function(request, response) {  
+        response.writeHeader(200, {"Content-Type": "text/html"});  
+        response.write(html);  
+        response.end();  
+    }).listen(8000);
+});
+
+/*<html>
+   <head>
+      <meta charset = "UTF-8" />
+      <title>TE-Appen</title>
+		<link> rel="stylesheet" type="text/css" href="App.css" </link>
+   </head>
+   <body>
+		<div id="wrapper"> 
+			<p>MASSA INFO</p>
+		</div>
+
+		<div id="wrapperlow">
+			<p>LÅG INFO</p>
+		</div>
+		
+   </body>
+   
+  
+</html>
+*/
