4b71af6e3c1cc1685c1de14a45c2256e24e1d8f4
Erica Ahlqvist <eriah648@student.liu.se>
2017-11-15 14:53:26 +0100

    Ni suger <3 :*

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+//import types from './types.json';
+import './app.css';
+
+
+class App extends Component {
+  render() {
+
+    return (
+
+      <div> hej </div>
+
+    );
+  }
+}
+
+export default App;
