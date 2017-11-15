1570078d0818850fb3bf31602550c38e5c067cce
Anna Furugård <annfu090@student.liu.se>
2017-11-15 17:16:12 +0100

    hej

--- src/app.js
+++ src/app.js
@@ -1,14 +1,64 @@
 import React, { Component } from 'react';
 //import types from './types.json';
 import './app.css';
+import {
+   BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
 
+const Home = () => (
+  <div>
+		<div id="wrapper"> 
+			<p>MASSA INFO</p>
+			
+		</div>
+
+		<div id="wrapperlow">
+			<p>LÅG INFO</p>
+			<Link id="button" to="mood">Mood</Link>
+			
+		</div>
+	
+  </div>
+)
+
+const mood = () => (
+  <div>
+		<div id="wrapper2"> 
+			<p>MASSA INFO</p>
+				
+			<Link id="button" to="Home">Home</Link>
+			
+		</div>
+	
+  </div>
+)
 
 class App extends Component {
+	
   render() {
 
     return (
 
-      <div> hej </div>
+   /*   <div id="root">
+   <head>
+      <meta charset = "utf-8" />
+      <title>TE-Appen</title>
+		<link rel="stylesheet" type="text/css" href="index.css"></link>
+   </head>*/
+  
+  <Router>
+    <div>
+      <hr/>
+
+      <Route exact path="/home" component={Home}/>
+	   <Route exact path="/mood" component={mood}/>
+      
+    </div>
+  </Router>
+   
+   
 
     );
   }
